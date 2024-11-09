39d3ad686a11501f67f59dbad6a201fa9276306c
romarsh <rom4rsh@gmail.com>
2024-11-09 21:08:07 +0300

    HW-13

--- src/Book.java
+++ src/Book.java
@@ -31,21 +31,20 @@ public class Book {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Book book = (Book) o;
-        return publicationYear == book.publicationYear && Objects.equals(bookName, book.bookName);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(bookName, publicationYear);
-    }
-
-    @Override
     public String toString() {
         return bookName + " " + publicationYear + " " + author;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return publicationYear == book.publicationYear && Objects.equals(bookName, book.bookName) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookName, publicationYear, author);
+    }
 }
