9938bcde640b565597a94d5f3215a95d5ffe3656
romarsh <rom4rsh@gmail.com>
2024-11-09 19:22:55 +0300

    HW-13

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-s
+        Author tolstoy = new Author("Марк", "Цукенберг");
+        Author author2 = new Author("Марк", "Цукенберг");
+        Book book = new Book("Война и мир", 1966, tolstoy);
+        Book book2 = new Book("Тяжко", 2024, author2);
+        book.setPublicationYear(2000);
+        System.out.println("Год = " + book.getPublicationYear());
+        System.out.println("book2 = " + book2.getPublicationYear() + " Название - " + book2.getBookName() + " автор- " + book2.getAuthor());
+        System.out.println(book);
+        System.out.println(book.equals(book2));
+        System.out.println(tolstoy.equals(author2));
+        System.out.println(author2);
+        System.out.println(book.hashCode());
+        System.out.println(tolstoy.hashCode());
     }
 }
